85ff86776243c50ab9f7e4d2c50031dedcd25b7e
Brett Hayes <bretterism@gmail.com>
2015-07-01 17:04:55 -0700

    Added ninject dependency resolver to be able to add bindings.

--- LeapList/App_Start/NinjectWebCommon.cs
+++ LeapList/App_Start/NinjectWebCommon.cs
@@ -61,6 +61,8 @@ namespace LeapList.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new
+            LeapList.Dependencies.NinjectDependencyResolver(kernel));
         }        
     }
 }
