5fc9b4ddba287b1de29ea0cb398659d8a26412bd
Brett Hayes <bretterism@gmail.com>
2015-07-06 16:01:11 -0700

    Got list controller to send a test list of results to the view. Removed the bootstrap defaults in the master layout.

--- LeapList/Controllers/ListController.cs
+++ LeapList/Controllers/ListController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LeapList.Models;
+using LeapList.Parse;
+using System.Xml;
 
 namespace LeapList.Controllers
 {
@@ -10,6 +13,11 @@ namespace LeapList.Controllers
     {
         public ActionResult Index()
         {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"https://corvallis.craigslist.org/search/bka?format=rss");
+            
+            // ViewBag.Items = List<CLItems>
+            ViewBag.Items = doc.GetItemList();
             return View();
         }
     }
