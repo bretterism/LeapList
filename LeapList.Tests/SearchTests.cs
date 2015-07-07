3d94b373a53104cc32ca7cdbd01f22b6a9f1a50e
Brett Hayes <bretterism@gmail.com>
2015-07-06 19:03:50 -0700

    Added search capabilites to CL feed. Added unit test for new method as well.

---
+++ LeapList.Tests/SearchTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeapList.Models;
+using LeapList.Parse;
+using LeapList.Search;
+
+namespace LeapList.Tests
+{
+    [TestClass]
+    public class SearchTests
+    {
+        [TestMethod]
+        public void TestSearchFor()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"../../Content/TestCLFeed.xml");
+
+            List<CLItem> items = new List<CLItem>();
+            items = doc.GetItemList();
+
+            IEnumerable<CLItem> filter = doc.SearchFor("Anatomy");
+            CLItem item = filter.ToList().FirstOrDefault();
+
+            Assert.IsNotNull(item);
+            Assert.AreEqual(item.Title, "Anatomy and Physiology Books $80");
+        }
+    }
+}
---
+++ LeapList/Search/Search.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using LeapList.Models;
+using LeapList.Parse;
+
+namespace LeapList.Search
+{
+    public static class SearchItems
+    {
+
+        /// <summary>
+        /// Searches for and returns a list of CLItem's that 
+        /// meet the search criteria.
+        /// </summary>
+        /// <param name="searchText">The search text.</param>
+        /// <returns>An enumerable of CLItems that met the search</returns>
+        public static IEnumerable<CLItem> SearchFor(this XmlDocument doc, string searchText)
+        {
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                return doc.GetItemList().Where(s => s.Title.Contains(searchText));
+            }
+
+            return Enumerable.Empty<CLItem>();
+        }
+    }
+}
