95626b7c10952181b04b594d108ea7d9d29d4b8a
Brett Hayes <bretterism@gmail.com>
2015-07-06 19:19:16 -0700

    Created the Search Criteria model. Added the view and controller for what will be the search.

---
+++ LeapList/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using LeapList.Models;
+using LeapList.Parse;
+using LeapList.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace LeapList.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Index(string searchText)
+        {
+            return View();
+        }
+    }
+}
---
+++ LeapList/Models/SearchCriteria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeapList.Models
+{
+    public class SearchCriteria
+    {
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Category { get; set; }
+        public string SearchText { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+
+    }
+}
