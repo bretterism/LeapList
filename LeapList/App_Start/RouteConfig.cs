7681efd39869c774208e0c4d3555d3a974116f47
Brett Hayes <bretterism@gmail.com>
2015-07-27 21:22:15 -0700

    Modified the default routeconfig and added a new routeconfig for the Profile view.

--- LeapList/App_Start/RouteConfig.cs
+++ LeapList/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace LeapList
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Profile",
+                url: "Profile/{action}/{id}",
+                defaults: new { controller = "Profile", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
