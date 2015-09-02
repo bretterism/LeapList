973b184f0d783bb5e276fffb7d1f918f55fe9d50
bretterism <bretterism@gmail.com>
2015-09-02 14:12:54 -0700

    DB Sql compare adding columns to categorysearch

--- LeapList.DB/Tables/CLItem.sql
+++ LeapList.DB/Tables/CLItem.sql
@@ -1,17 +1,17 @@
-﻿CREATE TABLE [CLItem] (
+﻿CREATE TABLE [dbo].[CLItem] (
     [ItemId]      INT             IDENTITY (1, 1) NOT NULL,
-    [SearchId]    INT             NOT NULL,
     [Title]       NVARCHAR (MAX)  NULL,
     [Description] NVARCHAR (MAX)  NULL,
     [Price]       DECIMAL (18, 2) NOT NULL,
     [Link]        NVARCHAR (MAX)  NULL,
     [Date]        DATETIME        NOT NULL,
-    CONSTRAINT [PK_dbo.CLItem] PRIMARY KEY CLUSTERED ([ItemId] ASC),
-    CONSTRAINT [FK_dbo.CLItem_dbo.SearchCriteria_SearchId] FOREIGN KEY ([SearchId]) REFERENCES [dbo].[SearchCriteria] ([SearchId]) ON DELETE CASCADE
+    [CategoryId]  INT             DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_dbo.CLItem] PRIMARY KEY CLUSTERED ([ItemId] ASC)
 );
 
 
+
+
 GO
-CREATE NONCLUSTERED INDEX [IX_SearchId]
-    ON [dbo].[CLItem]([SearchId] ASC);
+
 
--- LeapList.DB/dbo/Tables/CategorySearch.sql
+++ LeapList.DB/dbo/Tables/CategorySearch.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[CategorySearch] (
-    [CategoryId] INT          IDENTITY (1, 1) NOT NULL,
-    [SearchId]   INT          NOT NULL,
-    [Category]   NVARCHAR (3) NOT NULL,
+    [CategoryId] INT            IDENTITY (1, 1) NOT NULL,
+    [SearchId]   INT            NOT NULL,
+    [Category]   NVARCHAR (3)   NOT NULL,
+    [SearchLink] NVARCHAR (MAX) DEFAULT ('') NOT NULL,
     CONSTRAINT [PK_dbo.CategorySearch] PRIMARY KEY CLUSTERED ([CategoryId] ASC)
 );
+
+
 
 
 GO
